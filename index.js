8d7f8931cdaf96ae051754b5a56a0da657eb6030
chandan <b18050@students.iitmandi.ac.in>
2020-06-01 13:37:41 +0530

    Phonebook back-end step1

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const persons = [
+	{
+		name: "Arto Hellas",
+		number: "040-123456"
+	},
+	{
+		name: "Ada-Lovelace",
+		number: "39-34-234534"
+	},
+	{
+		name: "Dan Abramov",
+		number: "12-34-345"
+	},
+	{
+		name: "Harry Poppendieck",
+		number: "39-234-234"
+	}
+]
+	
+
+
+
+app.get('/',(request,response) => {
+	response.send('<h2> Hello World </h2>')
+})
+
+app.get('/api/persons',(request,response) => {
+	console.log(persons)
+	response.json(persons)
+})
+const port = 3001
+app.listen(port)
+console.log(`Server running at port ${port}`)
