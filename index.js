3b3a483327638356aa92d690d15472a37e71bd95
chandan <b18050@students.iitmandi.ac.in>
2020-06-03 19:42:35 +0530

    added

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ const errorHandler = (error, request, response, next) => {
   if(error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({error: 'malformatted id'})
   } else if(error.name === 'ValidationError') {
-    return response.status(400).send({error : 'not enough lines' })
+    return response.status(400).json({error : error.message })
   }
 
   next(error)
