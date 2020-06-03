30a274d1b177e5ea16343e0af675c77e7de41b5d
chandan <b18050@students.iitmandi.ac.in>
2020-06-03 15:36:36 +0530

    phonebook data base step2

--- index.js
+++ index.js
@@ -24,33 +24,6 @@ morgan.token('host', function(req, res) {
     return (JSON.stringify(person));
 });
 
-
-// let persons = [
-// 	{
-// 		id: 1,
-// 		name: "Arto Hellas",
-// 		number: "040-123456"
-// 	},
-// 	{
-// 		id: 2,
-// 		name: "Ada-Lovelace",
-// 		number: "39-34-234534"
-// 	},
-// 	{
-// 		id: 3,
-// 		name: "Dan Abramov",
-// 		number: "12-34-345"
-// 	},
-// 	{
-// 		id: 4,
-// 		name: "Harry Poppendieck",
-// 		number: "39-234-234"
-// 	}
-// ]
-	
-
-
-
 app.get('/',(request,response) => {
 	response.send('<h2> Hello World </h2>')
 })
@@ -65,39 +38,30 @@ app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id).then(person => {
     response.json(person)
   })
-  // const id = Number(request.params.id)
-  // const person = persons.find(person => person.id === id)
-  
-  // if (person) {
-  //   response.json(person)
-  // } else {
-  	
-  //   response.status(404).end()
-  // }
 })
 
-app.get('/info',(request,response) => {
-	console.log('IN INfo')
-	const len=persons.length
-	const date = new Date()
-	const info = `Phonebook has info for ${len} persons
-	<br> ${date}`
-	response.send(info)
+// app.get('/info',(request,response) => {
+// 	console.log('IN INfo')
+// 	const len=persons.length
+// 	const date = new Date()
+// 	const info = `Phonebook has info for ${len} persons
+// 	<br> ${date}`
+// 	response.send(info)
 
-})
+// })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
-})
+//   response.status(204).end()
+// })
 
-const getRandomInt = () => {
-	const Id = Math.floor(Math.random() * Math.floor(10000)); 
-	console.log(Id)
-	return Id
-  }
+// const getRandomInt = () => {
+// 	const Id = Math.floor(Math.random() * Math.floor(10000)); 
+// 	console.log(Id)
+// 	return Id
+//   }
 
 app.post('/api/persons', (request, response) => {
   
