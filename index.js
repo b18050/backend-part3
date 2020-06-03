f01a6e658bf39dae2ca570b045e312a30b3604e6
chandan <b18050@students.iitmandi.ac.in>
2020-06-03 17:54:51 +0530

    phonebook database step 6

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 
-app.use(express.json())  //Middle ware functions
+
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())  
+// app.use(logger)  //request.body is empty!
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :host')); // This is a modified version of morgan's tiny predefined format string.
 
@@ -34,11 +36,19 @@ app.get('/api/persons',(request,response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if(person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
+
+
 
 // app.get('/info',(request,response) => {
 // 	console.log('IN INfo')
@@ -50,18 +60,13 @@ app.get('/api/persons/:id', (request, response) => {
 
 // })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   persons = persons.filter(person => person.id !== id)
-
-//   response.status(204).end()
-// })
-
-// const getRandomInt = () => {
-// 	const Id = Math.floor(Math.random() * Math.floor(10000)); 
-// 	console.log(Id)
-// 	return Id
-//   }
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   
@@ -101,13 +106,41 @@ app.post('/api/persons', (request, response) => {
 
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  console.log(body)
+
+  const person = {
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id , person, {new:true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+//handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT ,() => {
   console.log(`Server running at port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+//handler of requests with result to errors
+app.use(errorHandler)
